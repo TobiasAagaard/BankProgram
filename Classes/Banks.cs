2ca65fbe1e1a679e9a96f804c19919254105c6b0
Tobias Aagaard Christiansen <tach@trifork.com>
2026-04-11 23:08:45 +0200

    Add and remove banks from bank list functions

--- Classes/Banks.cs
+++ Classes/Banks.cs
@@ -3,28 +3,65 @@ namespace BankProgram
     class Bank
     {
         private string name;
+        private List<Bank> banks;
         private List<Customer> customers;
 
-        public Bank(string name, List<Customer> customers)
+        public Bank(string name, List<Bank> banks, List<Customer> customers)
         {
             this.name = name;
+            this.banks = banks;
             this.customers = customers;
         }
 
         public string BankName { get { return name; } private set { name = value; } }
         public List<Customer> Customers { get { return customers; } private set { customers = value;}}
 
-        public void AddCustomer(Customer customer)
-        {
+        public List<Bank> Banks { get { return banks; } private set { banks = value;}}
 
+        public string AddCustomer(Bank bank, Customer customer)
+        {
+            if (!banks.Contains(bank))
+            {
+                throw new Exception("Bank does not exist");
+            }
+            else {
             customers.Add(customer);
+            return "Customer added successfully";
+            }ß
+
         }
 
-        public void RemoveCustomer(Customer customer)
+        public string RemoveCustomer(Customer customer)
         {
             customers.Remove(customer);
+            return "Customer removed successfully";
         }
 
+        public string AddBank(Bank bank)
+        {
+            if (banks.Contains(bank))
+            {
+                throw new Exception("Bank already exists");
+            }
+            else
+            {
+                banks.Add(bank);
+                return "Bank added successfully";
+            }
+        }
+        
+        public string RemoveBank(Bank bank)
+        {
+            if (!banks.Contains(bank))
+            {
+                throw new Exception("Bank deos not exist")
+            }
+            else
+            {
+                banks.Remove(bank);
+                return "Bank removed successfully";
+            }
+        }
 
     }
 }
