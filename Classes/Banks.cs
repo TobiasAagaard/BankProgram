8b23695c22283f78ef9dde7a578e63991b04cc17
Tobias Aagaard Christiansen <tach@trifork.com>
2026-04-13 21:21:24 +0200

    Add Advisor model with properties and customer relationship

--- Classes/Banks.cs
+++ Classes/Banks.cs
@@ -4,19 +4,19 @@ namespace BankProgram
     {
         private int id;
         private string name;
-        private List<Customer> customers;
+        private List<Advisor> advisors;
 
-        public Bank(int id, string name, List<Customer> customers)
+        public Bank(int id, string name, List<Advisor> advisors)
         {
             this.id = id;
             this.name = name;
-            this.customers = customers;
+            this.advisors = advisors;
         }
 
         public int BankId { get { return id; } private set { id = value; } }
 
         public string BankName { get { return name; } private set { name = value; } }
-        public List<Customer> Customers { get { return customers; } private set { customers = value;}}
+        public List<Advisor> Advisors { get { return advisors; } private set { advisors = value;}}
 
     }
 }
